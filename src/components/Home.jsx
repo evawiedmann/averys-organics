27f1d0ba94111737de50f8886f3b2071e241e66b
Student <example@example.com>
2020-02-12 13:24:24 -0800

    add knockout text to home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import '../Home.css';
 
 function Home(){
   return (
     <div>
-    <h1>Portland Area Farmers Markets</h1>
+      <div class="image-container">
+        <div class="text">AVERY'S ORGANICS</div>
+      </div>
      <Link to="/availableproducts">Produce by Season</Link> | <Link to="/marketschedule">Weekly Market Schedule</Link> | <Link to="/newmarketschedule">Staff: Add to Market Schedule</Link>
    </div>
   );
